611a377abe4641df2c30e47aeff855d8d351b1ae
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-01-12 16:18:43 +0600

    removed

--- client/src/main/java/tech/lapsa/insurance/calculation/EJBConstants.java
+++
@@ -1,7 +0,0 @@
-package tech.lapsa.insurance.calculation;
-
-public interface EJBConstants {
-
-    public static final String APPLICATION_NAME = "insurance-calculation-app";
-    public static final String MODULE_NAME = "insurance-calculation";
-}
